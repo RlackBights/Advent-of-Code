1ef799186dc288040f473aa952bc2975de053e81
RlackBights <hunor.n.b@gmail.com>
2023-12-16 13:08:39 +0100

    day 4, 5 progress

---
+++ Day 4 - Scratchcards/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent_of_Code
+{
+    class Card
+    {
+        public int[] winningNums { get; private set; }
+        public int[] ownNumbers { get; private set; }
+        public int cardValue { get; private set; }
+        public int cardId { get; set; }
+        public int copies { get; private set; }
+        public Card(string row)
+        {
+            winningNums = Array.ConvertAll(row.Split('|')[0].Trim().Replace("  ", " ").Split(' '), int.Parse);
+            ownNumbers = Array.ConvertAll(row.Split('|')[1].Trim().Replace("  ", " ").Split(' '), int.Parse);
+            copies = 1;
+            CalculateValue();
+        }
+
+        void CalculateValue()
+        {
+            cardValue = 0;
+            foreach (int own in ownNumbers)
+            {
+                if (winningNums.Contains(own))
+                {
+                    /*if (cardValue == 0)
+                    {
+                        cardValue = 1;
+                    } else
+                    {
+                        cardValue *= 2;
+                    }*/
+
+                    cardValue++;
+                }
+            }
+        }
+
+        public void AddCopy()
+        {
+            copies++;
+            CalculateValue();
+
+        }
+        
+
+    }
+    internal class Day4
+    {
+        static void Main(string[] args)
+        {
+            List<Card> cards = new List<Card>();
+            int rowCounter = 0;
+            foreach (string row in File.ReadAllLines("input.txt"))
+            {
+                Card c = new Card(row.Split(':')[1].Trim());
+                c.cardId = rowCounter++;
+                cards.Add(c);
+            }
+
+            /* 1. part solution
+            int sum = 0;
+
+            foreach (var item in cards)
+            {
+                sum += item.cardValue;
+            }
+
+            Console.WriteLine(sum);
+            */
+
+            foreach (var c in cards)
+            {
+                Console.WriteLine($"Adding {c.copies}*{c.cardValue/c.copies} copies");
+                for (int j = 0; j < c.copies; j++)
+                {
+                    for (int i = 1; i <= c.cardValue; i++)
+                    {
+                        Console.WriteLine($"Added copy to card #{c.cardId + i + 1}");
+                        cards[c.cardId + i].AddCopy();
+                    }
+                }
+            }
+
+            Console.WriteLine($"Ended up with {cards.Sum((c) => c.copies)} copies");
+
+        }
+    }
+}
---
+++ Day 5 - Seeds/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_5___Seeds
+{
+    class Seed
+    {
+        public int Id { get; private set; }
+        public int translatedValue { get; set; }
+
+        public Seed(int num)
+        {
+            Id = num;
+            translatedValue = num;
+        }
+    }
+
+    class Map
+    {
+        public int sourceStart { get; private set; }
+        public int destinationStart { get; private set; }
+        public int range { get; private set; }
+
+        public Map(string row)
+        {
+            int[] n = Array.ConvertAll(row.Trim().Split(' '), int.Parse);
+            destinationStart = n[0];
+            sourceStart = n[1];
+            range = n[2];
+        }
+
+        public int processNumber(int input)
+        {
+            if (input >= sourceStart && input < sourceStart + range)
+            {
+                return (input - sourceStart) + destinationStart;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+    }
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Seed> seeds = new List<Seed>();
+            List<Map> soil = new List<Map>();
+            List<Map> fertilizer = new List<Map>();
+            List<Map> water = new List<Map>();
+            List<Map> light = new List<Map>();
+            List<Map> temperature = new List<Map>();
+            List<Map> humidity = new List<Map>();
+            List<Map> location = new List<Map>();
+            List<List<Map>> maps = new List<List<Map>>() { soil, fertilizer, water, light, temperature, humidity, location };
+
+            int metaRowCounter = 0;
+
+            foreach (string row in File.ReadAllLines("input.txt"))
+            {
+                if (row == "") continue;
+                if (!int.TryParse(row[0].ToString(), out int x))
+                {
+                    if (metaRowCounter++ == 0)
+                    {
+                        foreach (int num in Array.ConvertAll(row.Split(':')[1].Trim().Split(' '), int.Parse))
+                        {
+                            seeds.Add(new Seed(num));
+                        }
+                    }
+                } else
+                {
+                    maps[metaRowCounter - 2].Add(new Map(row));
+                }
+            }
+
+
+
+            foreach (Seed seed in seeds)
+            {
+                foreach (var map in maps)
+                {
+                    foreach (var translation in map)
+                    {
+
+                    }
+                }
+            }
+
+
+        }
+    }
+}
