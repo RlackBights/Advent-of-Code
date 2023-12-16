4663695d53f7b12f994eef3f3bea7dbb0f111a88
RlackBights <hunor.n.b@gmail.com>
2023-12-16 21:54:07 +0100

    I hate day 5

--- Day 5 - Seeds/Program.cs
+++ Day 5 - Seeds/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -9,10 +10,10 @@ namespace Day_5___Seeds
 {
     class Seed
     {
-        public int Id { get; private set; }
-        public int translatedValue { get; set; }
+        public long Id { get; private set; }
+        public long translatedValue { get; set; }
 
-        public Seed(int num)
+        public Seed(long num)
         {
             Id = num;
             translatedValue = num;
@@ -21,38 +22,65 @@ namespace Day_5___Seeds
 
     class Map
     {
-        public int sourceStart { get; private set; }
-        public int destinationStart { get; private set; }
-        public int range { get; private set; }
+        public long sourceStart { get; private set; }
+        public long destinationStart { get; private set; }
+        public long range { get; private set; }
 
         public Map(string row)
         {
-            int[] n = Array.ConvertAll(row.Trim().Split(' '), int.Parse);
+            long[] n = Array.ConvertAll(row.Trim().Split(' '), long.Parse);
             destinationStart = n[0];
             sourceStart = n[1];
             range = n[2];
         }
 
-        public int processNumber(int input)
+        public long processNumber(long input)
         {
+            long result = -1;
             if (input >= sourceStart && input < sourceStart + range)
             {
-                return (input - sourceStart) + destinationStart;
+                result = (long)((input - sourceStart) + destinationStart);
             }
-            else
-            {
-                return -1;
-            }
+            return result;
         }
     }
 
-
+   
 
     internal class Program
     {
+        static long ProcessCurrentSeeds(List<long> seeds, List<List<Map>> maps, long closest)
+        {
+            for (int i = 0; i < seeds.Count; i++)
+            {
+                foreach (var map in maps)
+                {
+                    foreach (var translation in map)
+                    {
+                        if (translation.processNumber(seeds[i]) != -1)
+                        {
+                            seeds[i] = translation.processNumber(seeds[i]);
+                            Console.WriteLine("Translated seed");
+                            break;
+                        }
+                    }
+                }
+            }
+
+            foreach (long seed in seeds)
+            {
+                if (seed < closest)
+                {
+                    closest = seed;
+                }
+            }
+
+            return closest;
+        }
+
         static void Main(string[] args)
         {
-            List<Seed> seeds = new List<Seed>();
+            List<long> seeds = new List<long>();
             List<Map> soil = new List<Map>();
             List<Map> fertilizer = new List<Map>();
             List<Map> water = new List<Map>();
@@ -63,18 +91,29 @@ namespace Day_5___Seeds
             List<List<Map>> maps = new List<List<Map>>() { soil, fertilizer, water, light, temperature, humidity, location };
 
             int metaRowCounter = 0;
+            long closest = long.MaxValue;
 
             foreach (string row in File.ReadAllLines("input.txt"))
             {
                 if (row == "") continue;
-                if (!int.TryParse(row[0].ToString(), out int x))
+                if (!long.TryParse(row[0].ToString(), out long x))
                 {
                     if (metaRowCounter++ == 0)
                     {
-                        foreach (int num in Array.ConvertAll(row.Split(':')[1].Trim().Split(' '), int.Parse))
+                        long[] seedNums = Array.ConvertAll(row.Split(':')[1].Trim().Split(' '), long.Parse);
+                        Console.WriteLine(seedNums.Length);
+                        for (int i = 0; i < seedNums.Length; i += 2)
                         {
-                            seeds.Add(new Seed(num));
+                            seeds.Clear();
+                            for (int j = 0; j < seedNums[i + 1]; j++)
+                            {
+                                seeds.Add(seedNums[i] + j);
+                                Console.WriteLine($"Added {seedNums[i] + j}");
+                            }
+                            closest = ProcessCurrentSeeds(seeds, maps, closest);
                         }
+
+
                     }
                 } else
                 {
@@ -82,19 +121,7 @@ namespace Day_5___Seeds
                 }
             }
 
-
-
-            foreach (Seed seed in seeds)
-            {
-                foreach (var map in maps)
-                {
-                    foreach (var translation in map)
-                    {
-
-                    }
-                }
-            }
-
+            Console.WriteLine(closest);
 
         }
     }
